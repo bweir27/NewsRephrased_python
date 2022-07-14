d79c9549738f07db88f581fd5b3a742aadb0b033
Brian Weir <bweir27@gmail.com>
2022-07-14 01:24:57 -0400

    even more words to avoid

--- avoidterms.py
+++ avoidterms.py
@@ -1,10 +1,13 @@
 """
+TW//
+
 This is the list of words / phrases that, if seen within the text of a tweet,
 will prevent the Tweet from being put through the filter
 (to avoid potentially making 'satire' out of gruesome or triggering situations)
 
 This list will be regularly updated.
 """
+
 IGNORE_TERMS = [
     "shooting",
     "killed",
@@ -18,9 +21,13 @@ IGNORE_TERMS = [
     "rape",
     "rapist",
     "sexual assualt",
+    "sexually abuse",
+    "sexual abuse",
+    "sexual misconduct",
     "suicide",
     "torture",
     "anorexia",
     "anorexic",
-    "minor"
+    "minor",
+    "children",
 ]
